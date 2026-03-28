4669df96c8f7af9c8ea97815a01bfa3b0fbaa9a1
crtlvr <mirohil@outlook.com>
2026-03-28 15:54:40 +0200

    short button hider
    added short button and player side feed top topic selector hider

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ observer.observe(document.body, {
 
 hideShorts()
 function hideShorts() {
-    document.querySelectorAll("yt-horizontal-list-renderer, ytd-feed-filter-chip-bar-renderer, ytd-rich-shelf-renderer, ytd-guide-entry-renderer:has(a[title='Shorts'])").forEach(el => {
+    document.querySelectorAll("yt-chip-cloud-renderer, yt-horizontal-list-renderer, ytd-rich-section-renderer, ytd-feed-filter-chip-bar-renderer, ytd-rich-shelf-renderer, ytd-mini-guide-entry-renderer:has(a[title='Shorts']), ytd-guide-entry-renderer:has(a[title='Shorts'])").forEach(el => {
         el.style.display = "none"
     })
 }
